fbbfdb2754c0f9a11bca69399141f8ab94104c94
sabae <Sabaev-nik29@yandex.ru>
2025-07-17 22:18:36 +0300

    feat(path/courses): added new path courses

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,20 @@ import express from "express";
 const app = express()
 const port = 3000
 
+const db = {
+    courses: [
+        {id: 1, name: "Frontend"},
+        {id: 2, name: "Backend"},
+        {id: 3, name: "Designer"}
+    ]
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
+})
+
+app.get('/courses', (req, res) => {
+    res.json(db.courses)
 })
 
 app.listen(port, () => {
