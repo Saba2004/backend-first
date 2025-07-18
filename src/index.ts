bc4b96b73d9c8d3aab458d0d99dc3a24be90b92d
sabae <Sabaev-nik29@yandex.ru>
2025-07-18 21:15:47 +0300

    feat(db): adding new

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const db = {
     courses: [
         {id: 1, name: "Frontend"},
         {id: 2, name: "Backend"},
-        {id: 3, name: "Designer"}
+        {id: 3, name: "Designer"},
+        {id: 4, name: "QA"},
+        {id: 5, name: "Team-lead"},
     ]
 }
 
